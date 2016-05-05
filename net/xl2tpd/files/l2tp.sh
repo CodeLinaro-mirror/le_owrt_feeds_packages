fangsheng009/bsb002-system_feeds_packages

[xl2tpd] add blackhole route to void route loop when wan down
Taking wan down when l2tp tunnel established will result in
a route loop as following :
1, send packet on ppp interface ->
2, encapsulate above packet with a ppp header ->
3, encapsulate above packet with a l2tp header ->
4, encapsulate above packet with a udp header ->
5, encapsulate above packet with a ip header ->
6, select route for above ip packet, host route to vpn server(via wan)
   will be skipped because the wan was down ->
7, Then default route(via ppp interface) was used ->
8, send packet on ppp interface ->

Hence the system will hangup by a dead lock caused by above loop
A blackhole route added by command
"ip route add blackhole server_ip metric 1000"
will let the encapsulated tunnel packet hit this blackhole route
in step 7, and then get this packet dropped.

CRs-Fixed: 642843
Change-Id: I4dca47ecb08f4063f343a1fef2250825ef6bf839
Signed-off-by: Xiaoping Fan <xfan@codeaurora.org>

net/xl2tpd/files/l2tp.sh
@@ -29,6 +29,7 @@ proto_l2tp_setup() {
 	json_get_var server server && {
 		for ip in $(resolveip -t 5 "$server"); do
 			( proto_add_host_dependency "$config" "$ip" )
+			echo "$ip" >> /tmp/server.l2tp-${config}
 			serv_addr=1
 		done
 	}
@@ -80,6 +81,8 @@ proto_l2tp_setup() {
 proto_l2tp_teardown() {
 	local interface="$1"
 	local optfile="/tmp/l2tp/options.${interface}"
+
+	rm -f /tmp/server.l2tp-${interface}
 
 	case "$ERROR" in
 		11|19)

CodeLinaro-mirror/le_owrt_feeds_packages

Signed-off-by: Sachin Sundar <ssundar@codeaurora.org>

net/xl2tpd/files/l2tp.sh
@@ -30,6 +30,7 @@ proto_l2tp_setup() {
 	json_get_var server server && {
 		for ip in $(resolveip -t 5 "$server"); do
 			( proto_add_host_dependency "$interface" "$ip" )
+			echo "$ip" >> /tmp/server.l2tp-${interface}
 			serv_addr=1
 		done
 	}
@@ -114,6 +115,7 @@ proto_l2tp_teardown() {
 			echo "xl2tpd-control: Remove l2tp-$interface failed" >&2
 		}
 	fi
+	rm -f /tmp/server.l2tp-${interface}
 	# Wait for interface to go down
         while [ -d /sys/class/net/l2tp-${interface} ]; do
 		sleep 1